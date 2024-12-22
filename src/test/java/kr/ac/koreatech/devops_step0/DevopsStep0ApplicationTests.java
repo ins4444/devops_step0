87e5540e51d52e8650571245201b661eb0c77350
ins4444 <kh4444man@gmail.com>
2024-12-22 10:20:56 +0900

    test 실패 예제제

--- src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
+++ src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
@@ -1,5 +1,7 @@
 package kr.ac.koreatech.devops_step0;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,21 @@ class DevopsStep0ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	int add(int a, int b) {
+		return a + b;
+	}
+
+	@Test
+	void add_test1() {
+		int result = add(a:10, b:20);
+		assertEquals(expected:30, result);
+	}	
+
+	@Test
+	void add_test2()) {
+		int result = add(a:10, b:20);
+		assertEquals(expected:10, result);
+	}	
 
 }
